f6e378af22dd0781a67db7e8e771cb4538c3b9da
BhavaniChoudary <lakshmi.papasani96@msitprogram.net>
2018-08-27 19:55:41 +0530

    degrees_to_fahrenheit

---
+++ CSPP2-practice/m1/a3/degrees_to_fahrenheit.java
@@ -0,0 +1,8 @@
+class degrees_to_fahrenheit {
+    public static void main(String args[])
+    {
+        int degree = 36;
+        double farenheit = degree * 1.8 + 32;
+        System.out.println(farenheit);
+    }
+}
