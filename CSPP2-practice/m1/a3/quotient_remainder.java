168faa61bc76c3ea47c5487c805c2a3e70adaf29
BhavaniChoudary <lakshmi.papasani96@msitprogram.net>
2018-08-27 19:41:53 +0530

    quotient_remainder

---
+++ CSPP2-practice/m1/a3/quotient_remainder.java
@@ -0,0 +1,11 @@
+class quotient_remainder {
+    public static void main(String args[])
+    {
+        int a = 43;
+        int b = 10;
+        int c = a/b;
+        int d = a%b;
+        System.out.println("quotient: " + c);
+        System.out.println("remainder: " + d);
+    }
+}
