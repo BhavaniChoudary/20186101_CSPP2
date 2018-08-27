543cb44da7da999415213881faec5b5445a42180
BhavaniChoudary <lakshmi.papasani96@msitprogram.net>
2018-08-27 19:31:18 +0530

    Swap

---
+++ CSPP2-practice/m1/a3/Swap.java
@@ -0,0 +1,11 @@
+public class Swap{
+    public static void main(String[] args)
+    {
+        int a = 5;
+        int b = 4;
+        int c = a;
+        a = b;
+        b = c;
+        System.out.println(a + " " + b);
+    }
+}
