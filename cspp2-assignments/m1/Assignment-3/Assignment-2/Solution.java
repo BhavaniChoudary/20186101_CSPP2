b9b7dee2dfa151d6f5f6fd88d909544987701edc
BhavaniChoudary <lakshmi.papasani96@msitprogram.net>
2018-08-28 19:51:54 +0530

    area of triangle

---
+++ cspp2-assignments/m1/Assignment-3/Assignment-2/Solution.java
@@ -0,0 +1,49 @@
+/**
+ * { item_description }.
+ */
+import java.util.Scanner;
+/**
+ * Class for solution.
+ */
+public final class Solution {
+    /**
+     * Constructs the object.
+     */
+    private Solution() {
+    //
+    }
+    /**
+     * { function_description }.
+     *
+     * @param      base    The base
+     * @param      height  The height
+     */
+    static void areaOfTriangle(final int base, final int height) {
+        /**
+         * { var_description }.
+         */
+        //final float f = 0.5;
+        double area;
+        area = (0.5 * base * height);
+        System.out.println(area);
+
+    }
+    /**
+     * { function_description }.
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
+        /**
+         * { var_description }.
+         */
+        Scanner scan = new Scanner(System.in);
+        int base = scan.nextInt();
+        int height = scan.nextInt();
+        areaOfTriangle(base, height);
+    }
+    /*
+    Need to fill the areaOfTriangle function and print the output
+    of fahrenheit.
+    */
+}
