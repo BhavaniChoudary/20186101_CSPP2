46a2f863322d29b6e52e64c05866ec69b08e4416
BhavaniChoudary <lakshmi.papasani96@msitprogram.net>
2018-08-29 15:05:32 +0530

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

--- cspp2-assignments/m3/Assignment-2/Assignment-2/Solution.java
+++ cspp2-assignments/m3/Assignment-2/Assignment-2/Solution.java
@@ -1,6 +1,10 @@
-
+/**
+ * { item_description }
+ */
 import java.util.Scanner;
-
+/**
+ * Class for solution.
+ */
 public class Solution {
 /* Fill the main function to print the number of 7's between 1 to n*/
     public static void main(String[] args) {
