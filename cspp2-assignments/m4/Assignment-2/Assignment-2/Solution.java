335a4792e701ff0876cf4fa13b0ef969141f4389
BhavaniChoudary <lakshmi.papasani96@msitprogram.net>
2018-08-30 18:52:37 +0530

    "check_style_score -> 4 of 4 passed. style score: 0/1 "

---
+++ cspp2-assignments/m4/Assignment-2/Assignment-2/Solution.java
@@ -0,0 +1,61 @@
+/**
+ * { item_description }
+ */
+import java.util.Scanner;
+/**
+ * { item_description }.
+ */
+public final class Solution {
+
+    private Solution() {
+
+    }
+    /**
+     * { function_description }.
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
+        /**
+         * { var_description }.
+         */
+        Scanner sc = new Scanner(System.in);
+        int a1rows = sc.nextInt();
+        int a1colmn = sc.nextInt();
+        int[][] a1 = new int[a1rows][a1colmn];
+        for (int i = 0; i < a1rows; i++) {
+            for (int j = 0; j < a1colmn; j++) {
+                a1[i][j] = sc.nextInt();
+            }
+        }
+        int a2rows = sc.nextInt();
+        int a2colmn = sc.nextInt();
+        int[][] a2 = new int[a2rows][a2colmn];
+        for (int i = 0; i < a2rows; i++) {
+            for (int j = 0; j < a2colmn; j++) {
+                a2[i][j] = sc.nextInt();
+            }
+        }
+        int[][] sum = new int[a1rows][a1colmn];
+        if (a1.length == a2.length) {
+            for (int x = 0; x < a1rows; x++) {
+                for (int y = 0; y < a1colmn; y++) {
+                    sum[x][y] = a1[x][y] + a2[x][y];
+                }
+            }
+            for (int i = 0; i < a2rows; i++) {
+                for (int j = 0; j < a2colmn; j++) {
+                    if (j == a2colmn - 1) {
+                        System.out.print(sum[i][j]);
+                    } else {
+                        System.out.print(sum[i][j] + " ");
+                    }
+                }
+                System.out.println();
+            }
+
+        } else {
+            System.out.println("not possible");
+        }
+    }
+}
