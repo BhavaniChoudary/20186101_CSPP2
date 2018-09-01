a055a01e85496389869a72a76a719ea21acd61a6
BhavaniChoudary <lakshmi.papasani96@msitprogram.net>
2018-09-01 11:49:37 +0530

    "check_style_score -> 5 of 5 passed. style score: 1/1 "

--- cspp2-assignments/m6/Assignment-3/Solution.java
+++ cspp2-assignments/m6/Assignment-3/Solution.java
@@ -29,7 +29,7 @@ final class Solution {
             if (charArray[i] == oldChar) {
                 charArray[i] = newChar;
             }
-          }
+        }
         return charArray;
     }
     /**
