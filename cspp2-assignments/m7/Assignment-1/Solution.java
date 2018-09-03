f854bce514680ed9b574dd8be5e6df5cf30c0388
BhavaniChoudary <lakshmi.papasani96@msitprogram.net>
2018-09-03 19:22:34 +0530

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

--- cspp2-assignments/m7/Assignment-1/Solution.java
+++ cspp2-assignments/m7/Assignment-1/Solution.java
@@ -1,20 +1,45 @@
 import java.util.Scanner;
+/**
+ * Class for input validator.
+ */
 class InputValidator {
+    /**
+     * string variable used to take the input.
+     */
     private String data;
-    private int length;
-    public InputValidator(String input) {
+    /**
+     * len is used to calculate data
+     */
+    private int len;
+    /**
+     * Constructs the object.
+     *
+     * @param      input  The input
+     */
+    public InputValidator(final String input) {
         data = input;
-        length = data.length();
+        len = data.length();
     }
+    /**
+     * validateData returns the boolean value.
+     *
+     * @return     description_of_the_return_value.
+     */
     public boolean validateData() {
         int six = 6;
-        if (length < six) {
+        if (len < six) {
             return false;
         }
         return true;
     }
 }
+/**
+ * Class for solution.
+ */
 public class Solution {
+    /**
+     * Main function.
+     */
     public static void main(String args[]) {
         Scanner s = new Scanner(System.in);
         String input = s.next();
@@ -23,3 +48,7 @@ public class Solution {
 
     }
 }
+
+
+
+
