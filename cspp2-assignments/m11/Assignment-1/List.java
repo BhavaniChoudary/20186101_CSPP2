92ff4e7c00ca27956043e80b77bfecae2068586d
BhavaniChoudary <lakshmi.papasani96@msitprogram.net>
2018-09-07 17:54:58 +0530

    "check_style_score -> 3 of 3 passed. style score: 1/1 "

--- cspp2-assignments/m11/Assignment-1/List.java
+++ cspp2-assignments/m11/Assignment-1/List.java
@@ -432,7 +432,7 @@ public class List {
                     l.removeAll(a);
                 }
                 break;
-            case "subList": {
+            case "subList":
                 if (tokens.length != 2) {
                     break;
                 }
@@ -443,7 +443,6 @@ public class List {
                     System.out.println(object);
                 }
                 break;
-            }
             case "equals":
                 if (tokens.length == 2) {
                     String[] lt = tokens[1].split(",");
